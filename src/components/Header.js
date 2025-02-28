5c6901f79fe5c60f7140aaf1c1b30cc67cf375fc
osama999x <osamakhan99@hotmail.com>
2025-02-28 15:11:14 +0500

    Change favicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ import NavLinks from "./NavLinks";
 import logo from "../images/OK_3_Logo-1.png";
 
 const Header = () => {
-  return (
-    <header className="header">
-      <img className="logo" width={"60px"} src={logo} alt="Osama Logo" />
-      <NavLinks />
-    </header>
-  );
+    return (
+        <header className="header">
+            <img className="logo" width={"40px"} src={logo} alt="Osama Logo" />
+            <NavLinks />
+        </header>
+    );
 };
 
 export default Header;
