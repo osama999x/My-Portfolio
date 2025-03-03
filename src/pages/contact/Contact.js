1a0505b17d4f7c216f7ecd514505fc666df37cdd
osama999x <osamakhan99@hotmail.com>
2025-03-03 23:07:57 +0500

    Added existing project details

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -3,19 +3,21 @@ import SocialIcons from "../../components/SocialIcons";
 import ContactInfo from "../../components/ContactInfo";
 
 const Contact = ({ name, email, location }) => {
-  return (
-    <section className="contact">
-      <PageHeader title="Contact" description="Get in touch" />
-      <div className="contactWrap container">
-        <div className="row">
-          <div className="col-12 col-lg-6">
-            <ContactInfo name={name} location={location} email={email} />
-          </div>
-        </div>
-      </div>
-      <SocialIcons />
-    </section>
-  );
+    return (
+        <section className="contact">
+            <PageHeader title="Contact" description="Get in touch" />
+            <div className="contactWrap container">
+                <div className="row">
+                    <div className="col-12 col-lg-6">
+                        <ContactInfo name={name} location={location} email={email} />
+                    </div>
+                </div>
+            </div>
+            <SocialIcons />
+        </section>
+    );
 };
 
 export default Contact;
+
+
